4142b1cbeac95ece1231746a679c8ba9417e83c7
WinterCore <hogobbl@gmail.com>
2021-01-05 18:24:19 +0200

    Add remove projects and set them as active

--- src/ui/project.ts
+++ src/ui/project.ts
@@ -5,10 +5,12 @@ const $PROJECT_CONTAINER = document.querySelector("#projects") as HTMLDivElement
 type ProjectClickHandler = (project: Project) => void;
 
 class ProjectUI {
-    clickHandler: ProjectClickHandler
+    clickHandler: ProjectClickHandler;
+    removeHandler: ProjectClickHandler;
 
-    constructor(clickHandler: ProjectClickHandler) {
-        this.clickHandler = clickHandler
+    constructor(onClick: ProjectClickHandler, onRemove: ProjectClickHandler) {
+        this.clickHandler  = onClick;
+        this.removeHandler = onRemove;
     }
 
     add(project: Project): void {
@@ -17,6 +19,17 @@ class ProjectUI {
         div.textContent = project.title;
         div.addEventListener('click', () => this.clickHandler(project));
         div.dataset.id = `${project.id}`;
+
+        const removeIcon = document.createElement('span');
+        removeIcon.classList.add('material-icons');
+        removeIcon.innerHTML = 'close';
+
+        removeIcon.addEventListener('click', (e) => {
+            e.stopPropagation();
+            this.removeHandler(project)
+        });
+
+        div.appendChild(removeIcon);
 
         $PROJECT_CONTAINER.appendChild(div);
     }
@@ -28,6 +41,16 @@ class ProjectUI {
             return true;
         }
         return false;
+    }
+
+    setActive(id: number) {
+        const elems = Array.from($PROJECT_CONTAINER.querySelectorAll('.project'));
+        for (let elem of elems) {
+            elem.classList.remove('active');
+        }
+        const elem = $PROJECT_CONTAINER.querySelector(`.project[data-id="${id}"`);
+        if (elem)
+            elem.classList.add('active');
     }
 }
 
