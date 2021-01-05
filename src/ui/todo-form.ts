ca56f30ab2ef9c8bfd3f32edf39b5240b7d02fd1
WinterCore <hogobbl@gmail.com>
2021-01-05 18:23:52 +0200

    Add the ability to show & hide the todo form

--- src/ui/todo-form.ts
+++ src/ui/todo-form.ts
@@ -1,20 +1,31 @@
 import { TodoData } from '../lib/todo';
 
 type FormSubmitHandler = (todoData: TodoData) => void;
+type FormCloseHandler  = () => void;
 
-const $TODO_FORM = document.querySelector('#todo-form') as HTMLFormElement;
+const $TODO_FORM_OUTER = document.querySelector('.todo-form-outer') as HTMLDivElement;
+const $TODO_FORM_INNER = $TODO_FORM_OUTER.querySelector('.todo-form-inner') as HTMLDivElement;
+const $TODO_FORM       = document.querySelector('#todo-form') as HTMLFormElement;
+const $formTitle       = $TODO_FORM_OUTER.querySelector('h2') as HTMLHeadingElement;
 
-const $title = $TODO_FORM.querySelector('#todo-title') as HTMLInputElement;
+const $title       = $TODO_FORM.querySelector('#todo-title') as HTMLInputElement;
 const $description = $TODO_FORM.querySelector('#todo-description') as HTMLInputElement;
-const $dueDate = $TODO_FORM.querySelector('#todo-due-date') as HTMLInputElement;
-const $priority = $TODO_FORM.querySelectorAll('[name="priority"]') as NodeListOf<HTMLInputElement>;
+const $dueDate     = $TODO_FORM.querySelector('#todo-due-date') as HTMLInputElement;
+const $priority    = $TODO_FORM.querySelectorAll('[name="priority"]') as NodeListOf<HTMLInputElement>;
 
 class TodoForm {
   submitHandler: FormSubmitHandler;
+  closeHandler: FormCloseHandler;
 
-  constructor(onSubmit: FormSubmitHandler) {
+  constructor(onSubmit: FormSubmitHandler, onClose: FormCloseHandler) {
     this.submitHandler = onSubmit;
+    this.closeHandler = onClose;
     this.handleSubmit();
+
+    $TODO_FORM_OUTER.addEventListener('click', () => this.hide());
+    $TODO_FORM_INNER.addEventListener('click', (e) => e.stopPropagation());
+
+    this.updateTitle('Add Note');
   }
 
   fill(todo: TodoData) {
@@ -29,6 +40,19 @@ class TodoForm {
       $description.value = '';
       $dueDate.value = '';
       $priority[0].checked = true;
+  }
+
+  updateTitle(title: string) {
+      $formTitle.innerHTML = title;
+  }
+
+  show() {
+      $TODO_FORM_OUTER.classList.add('shown');
+  }
+
+  hide() {
+      $TODO_FORM_OUTER.classList.remove('shown');
+      this.closeHandler();
   }
 
   private handleSubmit() {
